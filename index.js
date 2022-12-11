14676c89a41a9d16498d8f54401988ad34f151ce
shahman-riaz <shahmanriaz07@gmail.com>
2022-12-12 01:20:41 +0600

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId || require('bson');
 require('dotenv').config()
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3p1eh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://mediHelp:medi77Help@cluster0.3p1eh.mongodb.net/mediHelp?retryWrites=true&w=majority`;
 
 const app = express()
 
